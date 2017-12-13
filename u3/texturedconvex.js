5adbf81cb52234cb8b6c8a27d2ccf572e44e6ce2
hakeris1010 <hakeris1010@gmail.com>
2017-12-13 13:06:11 +0200

    Working on TexturedConvex.

--- u3/texturedconvex.js
+++ u3/texturedconvex.js
@@ -4,6 +4,11 @@ static getDefault( key ){
     switch(key){
     case "image" : return "https://www.wpclipart.com/recreation/games/chess/chessboard.png";
     case "vertexCount" : return 1000;
+    case "material" : 
+        return new THREE.MeshPhongMaterial( { 
+            color: 0xffffff, 
+            side: THREE.DoubleSide 
+        } );
     case "shapeParams" : 
         var r = {
             type: "cone",
@@ -22,14 +27,24 @@ static getDefault( key ){
 }
 
 constructor( params ){
-    // Add properties
-    this.params = { 
+    var propertiesToCheck = [ "image", "material", "vertexCount", "shapeParams" ];
+    this.props = {};
+    /*this.params = { 
         image:       params ? params.image       : 0 || TexturedConvex.getDefault( "image" ),
         material:    params ? params.material    : 0 || TexturedConvex.getDefault( "material" ),
         vertexCount: params ? params.vertexCount : 0 || TexturedConvex.getDefault( "vertexCount" ),
         shapeParams: params ? params.shapeParams : 0 || TexturedConvex.getDefault( "shapeParams" )
-    };
-    var sp = this.params.shapeParams;
+    };*/
+
+    // Add Properties
+    propertiesToCheck.forEach( item => {
+        if( params ? typeof params[item] !== "undefined" : false ) 
+            this.props[item] = params[item];
+        else
+            this.props[item] = TexturedConvex.getDefault( item );
+    } ); 
+
+    var sp = this.props.shapeParams;
 
     // Generate convex points.
     var vertexes = [];
@@ -50,16 +65,21 @@ constructor( params ){
     // Create a ConvexGeometry.
     this.geometry = new THREE.ConvexGeometry( vertexes );
 
-    // Apply a texture to geometry.
-    this.applyTexture();
+    // Create a texture from image.
+    if(props.image)
+        this.setTextureFromImage( props.image );
+}
+
+setTextureFromImage( image ){
+
 }
 
 applyTexture(){
-
+    
 }
 
 getMesh(){
-
+    return new THREE.Mesh( this.geometry, this.material );
 }
 
 }
